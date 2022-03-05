6e7a2520aee264cb9ca36b8ee320173c23647365
bryanbill <emobrya@gmail.com>
2022-03-05 13:47:05 +0300

    feat:akan names - added the tabular akan names per index

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// Hold Akan names in the global namespace
+var akanNames = [
+    {
+        male: "Kwasi",
+        female: "Akosua"
+    },
+    {
+        male: "Kwadwo",
+        female: "Adwoa"
+    },
+    {
+        male: "Kwabena",
+        female: "Abenaa"
+    },
+    {
+        male: "Kwaku",
+        female: "Akua"
+    },
+    {
+        male: "Yaw",
+        female: "Yaa"
+    },
+    {
+        male: "Kofi",
+        female: "Afua"
+    },
+    {
+        male: "Kwame",
+        female: "Ama"
+    }
+];
+
+console.table(akanNames)
