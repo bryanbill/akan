7ea0de54f3712d0c1da72d0f45b807d2fe4f84df
bryanbill <emobrya@gmail.com>
2022-03-05 15:49:59 +0300

    feat:business logic - get name based on dob and gender

--- js/main.js
+++ js/main.js
@@ -30,4 +30,42 @@ var akanNames = [
     }
 ];
 
-console.table(akanNames)
+const day = "2001-01-18";
+
+const dayOfWeek = new Date(day).getDay();
+
+console.log(akanNames[dayOfWeek].male);
+
+/**
+ * Get the day of the week for date of birth
+ * @param {*} date 
+ * @returns int
+ */
+const getDay = (date) => new Date(date).getDay();
+
+/**
+ * Pick Akan Name based on the gender and day of week
+ * @param {*} gender 
+ * @param {*} dayOfWeek 
+ * @returns 
+ */
+const pickAkanName = (gender, dayOfWeek) => {
+
+    if (gender === 'male') {
+        return akanNames[dayOfWeek].male;
+    }
+    return akanNames[dayOfWeek].female;
+}
+
+const main = () => {
+    const dob = document.getElementById('dob').value;
+    const gender = document.getElementById('gender').value;
+    const name = document.getElementById('fullName').value;
+
+    const dayOfWeek = getDay(dob);
+    const akanName = pickAkanName(gender, dayOfWeek);
+
+    // Show Modal with the name
+
+
+}
